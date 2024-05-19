139c7ac8820eacaf97a15d7af03e9170f4b2662c
pawan-kavinda <138522618+pawan-kavinda@users.noreply.github.com>
2024-05-19 15:47:33 +0530

    transition

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,3 +1,129 @@
+// import React, { useState } from "react";
+// import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+// import { FaLinkedin } from "react-icons/fa";
+// import { FaGithub } from "react-icons/fa";
+// import { FaInstagram } from "react-icons/fa";
+// import { motion } from "framer-motion";
+
+// const motionIcons = (duration) => ({
+//   initial: { y: -10 },
+//   animate: {
+//     y: [5, -5],
+//     transition: {
+//       duration: duration,
+//       ease: "linear",
+//       repeat: Infinity,
+//       repeatType: "reverse",
+//     },
+//   },
+// });
+
+// const Navbar = () => {
+//   const [nav, setNav] = useState(false);
+
+//   const handleNav = () => {
+//     setNav(!nav);
+//   };
+
+//   return (
+//     <div className="bg-black h-[100px] text-xl text-white max-w-[1550px] mx-auto flex justify-between items-center md:px-20 px-12 fixed top-0 left-0 right-0 z-50">
+//       <h1 className="text-xl md:px-8 md:text-2xl font-bold bg-gradient-to-r from-slate-200 to-orange-600 bg-clip-text text-transparent">
+//         PAWAN <span className="hidden sm:hidden md:inline">KAVINDA</span>
+//       </h1>
+//       <ul className="hidden md:flex">
+//         <a href="https://github.com/pawan-kavinda">
+//           <motion.li
+//             variants={motionIcons(1)}
+//             initial="initial"
+//             animate="animate"
+//             className="p-5"
+//           >
+//             <FaGithub className="bg-gray-500 transform w-7 h-7 transition-transform duration-300 hover:scale-125 overflow-hidden shadow-lg shadow-yellow-100"></FaGithub>
+//           </motion.li>
+//         </a>
+
+//         <a href="https://www.instagram.com/pawan_kd_9/?hl=en">
+//           <motion.li
+//             variants={motionIcons(5)}
+//             initial="initial"
+//             animate="animate"
+//             className="p-5"
+//           >
+//             {/* <a href="#work">Work</a> */}
+//             <FaInstagram className="bg-red-800 w-7 h-7 transform transition-transform duration-300 hover:scale-125 overflow-hidden shadow-lg shadow-yellow-100"></FaInstagram>
+//           </motion.li>
+//         </a>
+//         <a href="https://www.linkedin.com/in/pawan-kavinda-6a3a37228/">
+//           <motion.li
+//             variants={motionIcons(10)}
+//             initial="initial"
+//             animate="animate"
+//             className="p-5"
+//           >
+//             {/* <a href="#contact">Contact</a> */}
+//             <FaLinkedin className="bg-blue-400 w-7 h-7 transform transition-transform duration-300 hover:scale-125 overflow-hidden shadow-lg shadow-yellow-100"></FaLinkedin>
+//           </motion.li>
+//         </a>
+//       </ul>
+//       <ul className="hidden font-bold md:text-2xl text-xl md:flex bg-gradient-to-r from-slate-200 to-orange-600 bg-clip-text text-transparent ">
+//         <li className="p-5">
+//           <a href="#about">About</a>
+          
+//         </li>
+//         <li className="p-5">
+//           <a href="#work">Work</a>
+//         </li>
+//         <li className="p-5">
+//           <a href="#contact">Contact</a>
+//         </li>
+//       </ul>
+//       <div className="block md:hidden mx-5  " onClick={handleNav}>
+//         {nav ? <AiOutlineClose  size={20} /> : <AiOutlineMenu />}
+//       </div>
+//       <div
+//         className={
+//           nav
+//             ? " z-10 fixed h-full left-0 top-0 w-[60%] bg-gray-800 ease-in-out duration-500"
+//             : "fixed left-[-100%]"
+//         }
+//       >
+//         <h1 className="text-3xl font-bold  bg-gradient-to-r from-slate-200 to-orange-600 bg-clip-text text-transparent m-4">Pawan Kavinda</h1>
+//         <ul className="p-8 text-2xl">
+//           <li className="p-5">
+//             <a href="#hero">Home</a>
+//           </li>
+//           <li className="p-5">
+//             <a href="#work">Work</a>
+//           </li>
+//           <li className="p-5">
+//             <a href="#contact">Contact</a>
+//           </li>
+//           <li className="p-5">
+//             <a href="#about">About</a>
+//           </li>
+//           <div className="flex">
+//             <li className="p-5">
+//             <a href="https://github.com/pawan-kavinda">
+//               <FaGithub className="bg-green-300"></FaGithub></a>
+//             </li>
+//             <li className="p-5">
+//               {/* <a href="#work">Work</a> */}
+//               <a href="https://www.instagram.com/pawan_kd_9/?hl=en">
+//               <FaInstagram className="bg-red-800"></FaInstagram></a>
+//             </li>
+//             <li className="p-5">
+//               {/* <a href="#contact">Contact</a> */}
+//               <a href="https://www.linkedin.com/in/pawan-kavinda-6a3a37228/">
+//               <FaLinkedin className="bg-blue-400"></FaLinkedin></a>
+//             </li>
+//           </div>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+// export default Navbar;
+
 import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { FaLinkedin } from "react-icons/fa";
@@ -25,6 +151,15 @@ const Navbar = () => {
     setNav(!nav);
   };
 
+  const smoothScroll = (e, id) => {
+    e.preventDefault();
+    const element = document.getElementById(id);
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
   return (
     <div className="bg-black h-[100px] text-xl text-white max-w-[1550px] mx-auto flex justify-between items-center md:px-20 px-12 fixed top-0 left-0 right-0 z-50">
       <h1 className="text-xl md:px-8 md:text-2xl font-bold bg-gradient-to-r from-slate-200 to-orange-600 bg-clip-text text-transparent">
@@ -49,7 +184,6 @@ const Navbar = () => {
             animate="animate"
             className="p-5"
           >
-            {/* <a href="#work">Work</a> */}
             <FaInstagram className="bg-red-800 w-7 h-7 transform transition-transform duration-300 hover:scale-125 overflow-hidden shadow-lg shadow-yellow-100"></FaInstagram>
           </motion.li>
         </a>
@@ -60,24 +194,26 @@ const Navbar = () => {
             animate="animate"
             className="p-5"
           >
-            {/* <a href="#contact">Contact</a> */}
             <FaLinkedin className="bg-blue-400 w-7 h-7 transform transition-transform duration-300 hover:scale-125 overflow-hidden shadow-lg shadow-yellow-100"></FaLinkedin>
           </motion.li>
         </a>
       </ul>
       <ul className="hidden font-bold md:text-2xl text-xl md:flex bg-gradient-to-r from-slate-200 to-orange-600 bg-clip-text text-transparent ">
-        <li className="p-5">
-          <a href="#about">About</a>
-          
+      <li className="p-5">
+          <a href="#hero" onClick={(e) => smoothScroll(e, "hero")}>Home</a>
         </li>
         <li className="p-5">
-          <a href="#work">Work</a>
+          <a href="#about" onClick={(e) => smoothScroll(e, "about")}>About</a>
         </li>
         <li className="p-5">
-          <a href="#contact">Contact</a>
+          <a href="#work" onClick={(e) => smoothScroll(e, "work")}>Work</a>
         </li>
+        <li className="p-5">
+          <a href="#contact" onClick={(e) => smoothScroll(e, "contact")}>Contact</a>
+        </li>
+       
       </ul>
-      <div className="block md:hidden mx-5  " onClick={handleNav}>
+      <div className="block md:hidden mx-5" onClick={handleNav}>
         {nav ? <AiOutlineClose  size={20} /> : <AiOutlineMenu />}
       </div>
       <div
@@ -103,18 +239,19 @@ const Navbar = () => {
           </li>
           <div className="flex">
             <li className="p-5">
-            <a href="https://github.com/pawan-kavinda">
-              <FaGithub className="bg-green-300"></FaGithub></a>
+              <a href="https://github.com/pawan-kavinda">
+                <FaGithub className="bg-green-300"></FaGithub>
+              </a>
             </li>
             <li className="p-5">
-              {/* <a href="#work">Work</a> */}
               <a href="https://www.instagram.com/pawan_kd_9/?hl=en">
-              <FaInstagram className="bg-red-800"></FaInstagram></a>
+                <FaInstagram className="bg-red-800"></FaInstagram>
+              </a>
             </li>
             <li className="p-5">
-              {/* <a href="#contact">Contact</a> */}
               <a href="https://www.linkedin.com/in/pawan-kavinda-6a3a37228/">
-              <FaLinkedin className="bg-blue-400"></FaLinkedin></a>
+                <FaLinkedin className="bg-blue-400"></FaLinkedin>
+              </a>
             </li>
           </div>
         </ul>
@@ -123,78 +260,3 @@ const Navbar = () => {
   );
 };
 export default Navbar;
-
-// import React, { useState } from "react";
-// import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-// import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
-
-// const Navbar = () => {
-//   const [nav, setNav] = useState(false);
-
-//   const handleNav = () => {
-//     setNav(!nav);
-//   };
-
-//   return (
-//     <nav className="bg-black text-white text-xl fixed top-0 left-0 right-0 z-50 px-20 md:flex md:justify-between md:items-center md:h-[100px]">
-//       <h1 className="text-3xl font-bold ml-4">PAWAN <span className="hidden md:inline">KAVINDA</span></h1>
-//       <ul className="md:flex">
-//         <li className="p-5">
-//           <FaGithub className="bg-gray-500 transform w-7 h-7 transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-yellow-100"></FaGithub>
-//         </li>
-//         <li className="p-5">
-//           <FaInstagram className="bg-red-800 w-7 h-7 transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-yellow-100"></FaInstagram>
-//         </li>
-//         <li className="p-5">
-//           <FaLinkedin className="bg-blue-400 w-7 h-7 transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-yellow-100"></FaLinkedin>
-//         </li>
-//       </ul>
-//       <ul className="md:flex">
-//         <li className="p-5">
-//           <a href="#about">About</a>
-//         </li>
-//         <li className="p-5">
-//           <a href="#work">Work</a>
-//         </li>
-//         <li className="p-5">
-//           <a href="#contact">Contact</a>
-//         </li>
-//       </ul>
-//       <div className="block md:hidden mx-5" onClick={handleNav}>
-//         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu />}
-//       </div>
-//       <div
-//         className={
-//           nav
-//             ? "z-40 fixed top-[100px] left-0 right-0 bg-black text-white text-xl md:hidden ease-in-out duration-500"
-//             : "hidden"
-//         }
-//       >
-//         <ul className="p-8 text-2xl">
-//           <li className="p-5">
-//             <a href="#about">About</a>
-//           </li>
-//           <li className="p-5">
-//             <a href="#work">Work</a>
-//           </li>
-//           <li className="p-5">
-//             <a href="#contact">Contact</a>
-//           </li>
-//         </ul>
-//         <div className="flex">
-//           <li className="p-5">
-//             <FaGithub className="bg-green-300"></FaGithub>
-//           </li>
-//           <li className="p-5">
-//             <FaInstagram className="bg-red-800"></FaInstagram>
-//           </li>
-//           <li className="p-5">
-//             <FaLinkedin className="bg-blue-400"></FaLinkedin>
-//           </li>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
--- frontend/src/Components/Skills.jsx
+++ frontend/src/Components/Skills.jsx
@@ -1,89 +1,133 @@
-import React from 'react'
+import React from "react";
 import react from "../assets/react.png";
 import flutter from "../assets/flutter.jpg";
 import asp from "../assets/asp.png";
 import sql from "../assets/sql.png";
 import tailwind from "../assets/tailwind.jpg";
 import node from "../assets/node.png";
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 import mongo from "../assets/mongo.svg";
 import fb from "../assets/firebase.png";
 import docker from "../assets/d.png";
 import azure from "../assets/azure.jpg";
 
-const motionIcons = (duration)=>({
-    initial:{y:-20},
-    animate:{
-        y:[10,-10],
-        transition:{
-            duration:duration,
-            ease:"linear",
-            repeat:Infinity,
-            repeatType:"reverse"
-        }
-    }
+const motionIcons = (duration) => ({
+  initial: { y: -20 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
 });
 
 const Skills = () => {
   return (
-    <div className='bg-black max-w-full mx-auto my-auto '>
-         <motion.h1 initial={{x:-100,opacity:0}} animate={{x:0,opacity:1}} transition={{duration:0.5,delay:1}}  className='bg-gradient-to-bl from-fuchsia-500 to-indigo-900 bg-clip-text text-transparent text-4xl sm:text-5xl font-bold  mx-4 p-3 text-center '>TECHNICAL STACK</motion.h1>
-         <div className='bg-black  text-gray-300 md:h-[350px] grid grid-cols-3 md:grid-cols-5 place-items-center md:flex md:justify-between md:items-center mx-auto max-w-[1200px] '>
-       
-        <motion.div variants={motionIcons(10)} initial="initial" animate="animate" className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
-         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]'>
-            <img src={react} width={100} height={100}  alt=''/>
-            {/* <p className='mt-2'>React</p> */}
+    <div className="bg-black max-w-full mx-auto my-auto ">
+      <motion.h1
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 1 }}
+        className="bg-gradient-to-bl from-fuchsia-500 to-indigo-900 bg-clip-text text-transparent text-4xl sm:text-5xl font-bold  mx-4 px-3 pb-8 text-center "
+      >
+        TECHNICAL STACK
+      </motion.h1>
+      <div className="bg-black  text-gray-300 md:h-[350px] grid grid-cols-3 md:grid-cols-5 place-items-center md:flex md:justify-between md:items-center mx-auto max-w-[1250px] md:gap-0 gap-11 ">
+        <motion.div
+          variants={motionIcons(10)}
+          initial="initial"
+          animate="animate"
+          className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
+         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]"
+        >
+          <img src={react} width={100} height={100} alt="" />
         </motion.div>
-        <motion.div variants={motionIcons(1)} initial="initial" animate="animate" className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
-         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px]  md:h-[120px] w-[100px] md:w-[120px]'>
-            <img src={flutter} width={100} height={100}  alt=''/>
-            
+        <motion.div
+          variants={motionIcons(1)}
+          initial="initial"
+          animate="animate"
+          className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
+         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px]  md:h-[120px] w-[100px] md:w-[120px]"
+        >
+          <img src={flutter} width={100} height={100} alt="" />
         </motion.div>
-        <motion.div variants={motionIcons(20)} initial="initial" animate="animate"  className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
-         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]'>
-            <img src={asp} width={100} height={100}  alt=''/>
-           
+        <motion.div
+          variants={motionIcons(20)}
+          initial="initial"
+          animate="animate"
+          className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
+         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]"
+        >
+          <img src={asp} width={100} height={100} alt="" />
         </motion.div>
-        <motion.div variants={motionIcons(1)} initial="initial" animate="animate"  className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
-         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]'>
-            <img src={sql} width={100} height={100} alt=''/>
-          
+        <motion.div
+          variants={motionIcons(1)}
+          initial="initial"
+          animate="animate"
+          className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
+         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]"
+        >
+          <img src={sql} width={100} height={100} alt="" />
         </motion.div>
-        <motion.div variants={motionIcons(15)} initial="initial" animate="animate"  className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
-         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]'>
-            <img src={tailwind} width={100} height={100}  alt=''/>
-           
+        <motion.div
+          variants={motionIcons(15)}
+          initial="initial"
+          animate="animate"
+          className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
+         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]"
+        >
+          <img src={tailwind} width={100} height={100} alt="" />
         </motion.div>
-        <motion.div variants={motionIcons(50)} initial="initial" animate="animate"  className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
-         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]'>
-            <img src={node} width={100} height={100}  alt=''/>
-            
+        <motion.div
+          variants={motionIcons(50)}
+          initial="initial"
+          animate="animate"
+          className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
+         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]"
+        >
+          <img src={node} width={100} height={100} alt="" />
         </motion.div>
-        <motion.div variants={motionIcons(2.5)} initial="initial" animate="animate"  className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
-         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]'>
-            <img src={mongo} width={100} height={100}  alt=''/>
-            
+        <motion.div
+          variants={motionIcons(2.5)}
+          initial="initial"
+          animate="animate"
+          className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
+         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px] md:w-[120px]"
+        >
+          <img src={mongo} width={100} height={100} alt="" />
         </motion.div>
-        <motion.div variants={motionIcons(2.5)} initial="initial" animate="animate"  className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
-         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px]  md:w-[120px]'>
-            <img src={fb} width={100} height={100}  alt=''/>
-           
+        <motion.div
+          variants={motionIcons(2.5)}
+          initial="initial"
+          animate="animate"
+          className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
+         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px]  md:w-[120px]"
+        >
+          <img src={fb} width={100} height={100} alt="" />
         </motion.div>
-        <motion.div variants={motionIcons(2.5)} initial="initial" animate="animate"  className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
-         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px]  md:w-[120px]'>
-            <img src={docker} width={100} height={100}  alt=''/>
-           
+        <motion.div
+          variants={motionIcons(2.5)}
+          initial="initial"
+          animate="animate"
+          className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
+         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px]  md:w-[120px]"
+        >
+          <img src={docker} width={100} height={100} alt="" />
         </motion.div>
-        <motion.div variants={motionIcons(2.5)} initial="initial" animate="animate"  className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
-         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px]  md:w-[120px]'>
-            <img src={azure} width={100} height={100}  alt=''/>
-           
+        <motion.div
+          variants={motionIcons(2.5)}
+          initial="initial"
+          animate="animate"
+          className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-pink-700 justify-center
+         group-container flex flex-col items-center m-4 sm:my-0 md:rounded-2xl md:border-4 border-neutral-800 border-2 rounded-xl h-[100px] md:h-[120px] w-[100px]  md:w-[120px]"
+        >
+          <img src={azure} width={100} height={100} alt="" />
         </motion.div>
+      </div>
     </div>
-    </div>
-    
-    
-  )
-}
+  );
+};
 export default Skills;
