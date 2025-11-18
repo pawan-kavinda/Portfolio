a88702a038ee2b7880ec30c5076ca699d34b1ef1
pawan-kavinda <pawankavinda09@gmail.com>
2025-11-18 13:50:28 +0530

    typo

--- frontend/src/Components/Work.jsx
+++ frontend/src/Components/Work.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-
-// Images
 import ai from "../assets/ml.jpg";
 import gc from "../assets/Projects/gc.jpg";
 import ems from "../assets/Projects/ems.jpg";
